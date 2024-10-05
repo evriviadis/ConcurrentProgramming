0f85e7e5120b84d8a947aa0cd373e7e1f7f8f708
vaggelisplytas <vaggelis.plytas@gmail.com>
2024-10-05 12:33:42 +0300

    testcommit

--- FIFO_pipes/library.c
+++ FIFO_pipes/library.c
@@ -1,18 +1,27 @@
+#include "library.h"
 #include <stdio.h>
 #include <pthread.h>
 
+// creates and opens a pipe for r/w | returns the pipe id
 int pipe_open(int size) {
     
 };
 
-int pipe_write(int p, char c){
+// writes 1 byte to the pipe | returns -1 if there is no
+// opened pipe with that id, otherwise 1
+int pipe_write(int p, char c) {
 
 };
 
-int pipe_writeDone(int p){
+// closes the pipe for writing | returns -1 if there is no
+// opened pipe with that id, otherwise 1
+int pipe_writeDone(int p) {
 
 };
 
-int pipe_read(int p, char *c){
+// reads and removes 1 byte from the pipe | returns 1 for 
+// success, 0 if pipe is empty or closed, -1 if there is 
+// no opened pipe with that id
+int pipe_read(int p, char *c) {
 
 };
--- FIFO_pipes/library.h
+++ FIFO_pipes/library.h
@@ -1,21 +1,13 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
-
 typedef struct {
-    int size;
-    int id;
-    int write_edge;
-    int read_edge;
+    int size, id, write_edge, read_edge;
 } pipeT;
 
-
-
-
-
-int pipe_open(int size);
-int pipe_write(int p, char c);
-int pipe_writeDone(int p);
-int pipe_read(int p, char *c);
+extern int pipe_open(int size);
+extern int pipe_write(int p, char c);
+extern int pipe_writeDone(int p);
+extern int pipe_read(int p, char *c);
 
 #endif
