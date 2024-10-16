d90f2d87069ccdbde44816fb93610b45c3b3c5d1
Katerinatsia <150055070+Katerinatsia@users.noreply.github.com>
2024-10-16 15:36:38 +0300

    Update library.h
    GEIAAA

--- External_mergesort/library.h
+++ External_mergesort/library.h
@@ -1,13 +1,15 @@
-#ifndef LIBRARY_H
 #define LIBRARY_H
 
 #define MIN_SIZE 4
+
+/*ARRAY TO BE SORTED*/
 typedef struct {
     int *array;
     int length;
-    int left;   // Start index of the array portion
+    int left;   //array's start index
 } arrayT;
 
+/*FUNCTIONS' PROTOTYPES*/
 extern int my_read(int fd, void *buffer, int size, int *left);
 extern void merge(int arr[], int left, int mid, int right);
 extern void mergeSort(int arr[], int left, int right);
